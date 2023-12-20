6df1097f876b0c78c4e86acc3e1392443d2e5fdd
Feellings <ickyazer@gmail.com>
2023-12-20 21:39:46 +0100

    updates

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1453,7 +1453,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/Haiilyyy/NUSUSMPLauncher//releases.atom',
+        url: 'https://github.com/Haiilyyy/NUSUSMPLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
